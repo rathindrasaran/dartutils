066e08afbeb940cf8f3c8a9042bedb9524924812
Rathindra Saran <mr.rathindra.saran@gmail.com>
2023-03-26 19:25:36 +0530

    added MySQLHelper

---
+++ MySQLHelper.dart
@@ -0,0 +1,59 @@
+import 'package:mysql1/mysql1.dart';
+
+class MySQLHelper {
+  late MySqlConnection _connection;
+
+  MySQLHelper({
+    required String host,
+    required int port,
+    required String user,
+    required String password,
+    required String db,
+  }) {
+    _connection = MySqlConnection(
+      settings: ConnectionSettings(
+        host: host,
+        port: port,
+        user: user,
+        password: password,
+        db: db,
+      ),
+    );
+  }
+
+  Future<void> connect() async {
+    await _connection.open();
+  }
+
+  Future<void> disconnect() async {
+    await _connection.close();
+  }
+
+  Future<List<Map<String, dynamic>>> select(String table,
+      {List<String>? columns, String? where, int? limit}) async {
+    String query = 'SELECT ${columns != null ? columns.join(',') : '*'} '
+        'FROM $table '
+        '${where != null ? 'WHERE $where' : ''} '
+        '${limit != null ? 'LIMIT $limit' : ''}';
+    var result = await _connection.query(query);
+    return result.toList();
+  }
+
+  Future<void> update(String table, Map<String, dynamic> values,
+      {String? where}) async {
+    String query = 'UPDATE $table SET '
+        '${values.entries.map((e) => '${e.key} = ${e.value}').join(',')} '
+        '${where != null ? 'WHERE $where' : ''}';
+    await _connection.query(query);
+  }
+
+  Future<void> delete(String table, {String? where}) async {
+    String query = 'DELETE FROM $table '
+        '${where != null ? 'WHERE $where' : ''}';
+    await _connection.query(query);
+  }
+
+  Future<void> execute(String query) async {
+    await _connection.query(query);
+  }
+}
